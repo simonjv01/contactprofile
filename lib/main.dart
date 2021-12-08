d0db7b5f969a941a1b28449428ef312862212ec4
Simon Vargas <sjvargas01@gmail.com>
2021-12-07 20:24:22 -0500

    updated main contact page file

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,8 @@ class ContactProfile extends
                 SizedBox(
                   //Container is stretched horizontally
                   width: double.infinity,
-                  //Image is fetched from url
+                  //Image is fetched from assets folder use Image.network
+                  //to fetch with url, "https:..."
                   child: Image.asset(
                       'assets/images/altumcode-unsplash.jpg'),
                   height: 320,
@@ -68,7 +69,56 @@ class ContactProfile extends
                 //NEW CODE: Adding Divider below the display name
                 const Divider(
                   color: Colors.grey,
-                )
+                ),
+                //NEW CODE: Container for action items
+                Container(
+                  margin: const EdgeInsets.only(top: 8, bottom: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      //"Call" action item
+                      buildCallButton(),
+
+                      //"Text" action item
+                      buildTextButton(),
+
+                      //"Video" action item
+                      buildVideoCallButton(),
+
+                      //"Email" action item
+                      buildEmailButton(),
+
+                      //"Directions" action item
+                      buildDirectionsButton(),
+
+                      //"Pay" action item
+                      buildPayButton(),
+                    ],
+                  ),
+                ),
+                //NEW CODE: Adding Divider
+                const Divider(
+                  color: Colors.grey,
+                ),
+                //NEW CODE: Adding contact's phone numbers
+                mobilePhoneListTile(),
+                otherPhoneListTile(),
+
+                //NEW CODE: Adding 'Divider' widget
+                const Divider(
+                  color: Colors.grey,
+                ),
+
+                //Adding email list
+                emailListTile(),
+
+                //Adding Divider
+                const Divider(
+                  color: Colors.grey,
+                ),
+
+                //Adding address tile
+                addressListTile(),
               ],
             )
           ]
@@ -76,4 +126,149 @@ class ContactProfile extends
       ),
     );
   }
+  //Adding "Call" action item
+  Widget buildCallButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.call,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Call"),
+      ],
+    );
+  }
+
+  //Adding "Text" action item
+  Widget buildTextButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.message,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Text"),
+      ],
+    );
+  }
+  //Adding "Video" action item
+  Widget buildVideoCallButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.video_call,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Video"),
+      ],
+    );
+  }
+  //Adding "Email" action item
+  Widget buildEmailButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.email,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Email")
+      ],
+    );
+  }
+  //Adding "Directions" action item
+  Widget buildDirectionsButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.directions,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Directions")
+      ],
+    );
+  }
+  //Adding "Pay" action item
+  Widget buildPayButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.attach_money,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Pay")
+      ],
+    );
+  }
+
+  //NEW CODE: Adding "Mobile phone number" item
+  Widget mobilePhoneListTile() {
+    return ListTile(
+      leading: const Icon(Icons.call),
+      title: const Text("330-803-3390"),
+      subtitle: const Text("mobile"),
+      trailing: IconButton(
+        icon: const Icon(Icons.message),
+        color: Colors.indigo.shade500,
+        onPressed: () {},
+      ),
+    );
+  }
+
+  //Adding "Other Phone Number" item
+  Widget otherPhoneListTile() {
+    return ListTile(
+      leading: const Text(""),
+      title: const Text("440-44-3390"),
+      subtitle: const Text("other"),
+      trailing: IconButton(
+        icon: const Icon(Icons.message),
+        color: Colors.indigo.shade500,
+        onPressed: () {},
+    ),
+    );
+
+  }
+  //Adding "email list" item
+  Widget emailListTile() {
+    return  const ListTile(
+      leading: Icon(Icons.email),
+      title: Text("vargas@vargas.com"),
+      subtitle: Text("work"),
+    );
+  }
+
+  //Adding "address" item
+  Widget addressListTile() {
+    return  ListTile(
+      leading: const Icon(Icons.location_on),
+      title: const Text("234 Sunset St, Burlingame"),
+      subtitle: const Text("home"),
+      trailing: IconButton(
+        icon: const Icon(Icons.directions),
+        color: Colors.indigo.shade500,
+        onPressed: () {},
+      ),
+    );
+  }
+
+
+
 }
